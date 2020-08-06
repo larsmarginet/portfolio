e3ece4a416870b636b0136902b20754827b1dd54
larsmarginet <lars.marginet@student.howest.be>
2020-08-06 20:06:09 +0200

    Developed contact form Close #7

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,64 +12,160 @@ const Contact = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [globalMessage, setGlobalMessage] = useState({
+        message: "",
+        green: false
+    });
+    const [nameMessage, setNameMessage] = useState({
+        message: "",
+        green: false
+    });
+    const [emailMessage, setEmailMessage] = useState({
+        message: "",
+        green: false
+    });
+    const [messageMessage, setMessageMessage] = useState({
+        message: "",
+        green: false
+    });
 
 
-    
+    const validateName = () => {
+        if(!name || name === "" || name === undefined || name === null) {
+            setNameMessage({
+                message: "Required",
+                green: false
+            });
+        } else {
+            setNameMessage({
+                message: "Nice name",
+                green: true
+            });
+        }
+    }
 
+    const validateEmail = () => {
+        if(!email || email === "" || email === undefined || email === null) {
+            setEmailMessage({
+                message: "Required",
+                green: false
+            });
+        } else if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) === false){
+            setEmailMessage({
+                message: "Invalid email",
+                green: false
+            });
+        } else {
+            setEmailMessage({
+                message: "Thanks",
+                green: true
+            });
+        }
+    }
+
+    const validateMessage = () => {
+        if(!message || message === "" || message === undefined || message === null) {
+            setMessageMessage({
+                message: "Required",
+                green: false
+            });
+        } else {
+            setMessageMessage({
+                message: "",
+                green: true
+            });
+        }
+    }
+
+    const validate = () => {
+        validateName();
+        validateEmail();
+        validateMessage();
+        if(nameMessage.green === false || emailMessage.green === false || messageMessage.green === false) {
+            return false;
+        } 
+        return true;
+    }
 
     const handleSubmit = e => {
         e.preventDefault();
         const form = e.target;
-        fetch('/', {
-            method: 'POST',
-            headers: { "Content-Type": "application/x-www-form-urlencoded" }, 
-            body: encode({
-                'form-name': form.getAttribute('name'),
-                name: name,
-                email: email,
-                message: message
+        const valid = validate();
+        if(valid) {
+            fetch('/', {
+                method: 'POST',
+                headers: { "Content-Type": "application/x-www-form-urlencoded" }, 
+                body: encode({
+                    'form-name': form.getAttribute('name'),
+                    name: name,
+                    email: email,
+                    message: message
+                })
             })
-        })
-        .then((res) => console.log(res))
-        .catch((error) => console.log(error))
-        // console.log(e);
-        // let formData = new FormData();
-        // formData.append('name', name);
-        // formData.append('email', email);
-        // formData.append('message', message);
-        // console.log(new URLSearchParams(formData).toString())
-        // fetch(e.target.getAttribute('action'), {
-        //     method: 'POST',
-        //     headers: {
-        //       'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-        //       'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-        //     },
-        //     body: new URLSearchParams(formData).toString()
-        //   })
-        //   .then(res => {
-        //     if (res) {
-        //      console.log(res);
-        //     }
-        //   });
+            .then(() => {
+                setGlobalMessage({
+                    message: "Thanks for your mail!",
+                    green: true
+                });
+                setTimeout(() => setGlobalMessage(''), 6000)
+                setName('');
+                setEmail('');
+                setMessage('');
+                setNameMessage({
+                    message: "",
+                    green: false
+                });
+                setEmailMessage({
+                    message: "",
+                    green: false
+                });
+                setMessageMessage({
+                    message: "",
+                    green: false
+                });
+            })
+            .catch((error) => {
+                setGlobalMessage({
+                    message: "Oops... Something went wrong.",
+                    green: false
+                });
+                setTimeout(() => setGlobalMessage(''), 6000)
+                console.log(error)
+            })
+        }
     }
 
     return (
-        <form onSubmit={handleSubmit} className={style.form} name="contact" action="/" method="POST" netlify-honeypot="bot-field" data-netlify="true">
-            <div className="visually-hidden">
-                <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-            </div>
+        <>
+            {globalMessage.message ? 
+            <div style={globalMessage.green ? {backgroundColor: "#00FFB2"} : {backgroundColor: "#2F2873", color: "#F24F5B"}} className={style.globalMessage}>
+                <p>{globalMessage.message}</p>
+            </div> : null}
+            <form onSubmit={handleSubmit} className={style.form} name="contact" action="/" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+                <div className="visually-hidden">
+                    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+                </div>
 
-            <label className={style.formLabel} htmlFor="name">Name</label>
-            <input className={style.formInput} id="name" type="text" name="name" value={name} onChange={e => setName(e.currentTarget.value)}/>
+                <label className={style.formLabel} htmlFor="name">Name</label>
+                <div className={style.formInputWrapper}>
+                    <p className={style.message} style={nameMessage.green ? {color: "#00FFB2"} : null}><span>{nameMessage.message}</span></p>
+                    <input style={nameMessage.green ? {borderColor: "#00FFB2"} : null} className={style.formInput} id="name" type="text" name="name" value={name} onChange={e => setName(e.currentTarget.value)} onBlur={validateName}/>
+                </div>
 
-            <label className={style.formLabel} htmlFor="email">Email</label>
-            <input className={style.formInput}id="email" type="text" name="email" value={email} onChange={e => setEmail(e.currentTarget.value)}/>
+                <label className={style.formLabel} htmlFor="email">Email</label>
+                <div className={style.formInputWrapper}>
+                    <p className={style.message} style={emailMessage.green ? {color: "#00FFB2"} : null}><span>{emailMessage.message}</span></p>
+                    <input style={emailMessage.green ? {borderColor: "#00FFB2"} : null} className={style.formInput}id="email" type="text" name="email" value={email} onChange={e => setEmail(e.currentTarget.value)} onBlur={validateEmail}/>
+                </div>
 
-            <label htmlFor="message" className={style.formLabel}>Message</label>
-            <textarea className={style.formInput} rows="8" id="message" name="message" value={message} onChange={e => setMessage(e.currentTarget.value)}></textarea>
-
-            <button className={style.formSubmit} type="submit">Send message</button>
-        </form>
+                <label htmlFor="message" className={style.formLabel}>Message</label>
+                <div className={style.formInputWrapper}>
+                    <p className={style.message}><span>{messageMessage.message}</span></p>
+                    <textarea style={messageMessage.green ? {borderColor: "#00FFB2"} : null} className={style.formInput} rows="8" id="message" name="message" value={message} onChange={e => setMessage(e.currentTarget.value)} onBlur={validateMessage}></textarea>
+                </div>
+                <button className={style.formSubmit} type="submit">Send message</button>
+            </form>
+        </>
     )
 }
 
