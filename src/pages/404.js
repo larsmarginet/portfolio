6422680a1a5f3a8521995d7fc6c2838880e0ca49
larsmarginet <lars.marginet@student.howest.be>
2020-08-10 11:47:53 +0200

    developed 404 page Close #13

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
+import style from "./404.module.css"
 
 export default function About() {
   return (
     <Layout>
-      <p>404</p>
+      <section className={style.section}>
+        <h2 className={style.sectionTitle}>404</h2>
+        <p className={style.sectionText}>Page not found.</p>
+        <Link className={style.sectionLink} to="/">Home</Link>
+      </section>
     </Layout>
   )
 }
