e8b142889274eb37658713d2934fe9be1eed28f7
larsmarginet <lars.marginet@student.howest.be>
2020-08-11 17:04:10 +0200

    IE warning

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,10 @@
 import "./src/styles/index.css"
 
+
+let showBrowserAlert = () => {
+    if (/Trident\/|MSIE/.test(window.navigator.userAgent)) {
+        alert('Unsupported Browser! This website will offer limited functionality in this browser. We only support the recent versions of major browsers like Chrome, Firefox, Safari, and Edge.');
+    }
+};
+
+document.addEventListener('DOMContentLoaded', showBrowserAlert);
